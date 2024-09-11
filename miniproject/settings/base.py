31f1c49f65aaf1c369c3f14abdcb07422787e06c
hwangtate <ghkdxodud10253504@gmail.com>
2024-09-11 15:27:57 +0900

    ✨Feat: user 모델 정의
    - settings에 user app 추가
    - models 모델 정의
    - manager 정의
    - make migrations

--- miniproject/settings/base.py
+++ miniproject/settings/base.py
@@ -16,6 +16,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # own apps
+    "users.apps.UsersConfig",
 ]
 
 MIDDLEWARE = [
@@ -86,3 +88,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
