3b77344c136ed1ba3a7b26384c86f0286bd29884
hwangtate <ghkdxodud10253504@gmail.com>
2024-09-12 10:54:36 +0900

    🐛Fix: ci 에러 수정
    - 에러는 전 커밋과 같음

--- miniproject/settings/development.py
+++ miniproject/settings/development.py
@@ -13,7 +13,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
         "USER": "postgres",
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
         "HOST": "localhost",
         "PORT": "5432",
     }
