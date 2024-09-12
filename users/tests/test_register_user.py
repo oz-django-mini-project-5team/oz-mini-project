5f3618b31ba36536c4524a0d5c04bad57ddb2570
hwangtate <ghkdxodud10253504@gmail.com>
2024-09-12 16:48:21 +0900

    ✅Test: register user api 추가
    - 테스트 코드 작성

--- users/tests/test_register_user.py
+++ users/tests/test_register_user.py
@@ -1,1 +1,19 @@
+from django.urls import reverse
+from rest_framework import status
 from rest_framework.test import APITestCase
+
+
+class TestRegisterUserAPI(APITestCase):
+
+    def setUp(self) -> None:
+        self.url = reverse("user_register")
+        self.valid_data = {
+            "name": "Test User",
+            "email": "testuser@example.com",
+            "password": "Qwas1287!",
+            "password2": "Qwas1287!",
+        }
+
+    def test_register_user_success(self) -> None:
+        response = self.client.post(self.url, self.valid_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
