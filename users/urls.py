c163fe086515f8cfdd37bdebb6a9a772fad21781
hwangtate <ghkdxodud10253504@gmail.com>
2024-09-13 08:16:56 +0900

    💄Style: run isort

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView, TokenVerifyView)
 
 from users import views
 
