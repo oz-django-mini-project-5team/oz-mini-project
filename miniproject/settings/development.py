08caf81a11331e9a954b66742f46d0cc1efcad47
hwangtate <ghkdxodud10253504@gmail.com>
2024-09-13 10:15:52 +0900

    🐛Fix: ci 에러 수정
    - defalut 값

--- miniproject/settings/development.py
+++ miniproject/settings/development.py
@@ -13,7 +13,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
         "USER": "postgres",
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "1025"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
         "HOST": "localhost",
         "PORT": "5432",
     }
