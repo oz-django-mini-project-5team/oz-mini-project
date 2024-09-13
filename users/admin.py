f74eac9b2dbbc8bdc0983a0839197611bc5d2046
hwangtate <ghkdxodud10253504@gmail.com>
2024-09-13 11:12:56 +0900

    ✨Feat: user admin 기능 추가
    - 미션 3-3 요구사항에 맞추었음

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from users.models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("email", "name", "phone_number", "is_superuser", "is_active")
+
+    search_fields = ("email", "name", "phone_number")
+
+    list_filter = ("is_superuser", "is_active")
+
+    list_display_links = ("email",)
+
+    readonly_fields = ("is_superuser",)
