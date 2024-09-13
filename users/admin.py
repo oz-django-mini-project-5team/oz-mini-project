3c07a676b7c8cb501d5167b4981a54dff761577f
hwangtate <ghkdxodud10253504@gmail.com>
2024-09-13 11:20:27 +0900

    💄Style: black, isort, mypy

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 
 from users.models import User
 
 
+# mypy: ignore-errors
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ("email", "name", "phone_number", "is_superuser", "is_active")
