6291b355e0f61bedff4efc11de3329db432d2331
hwangtate <ghkdxodud10253504@gmail.com>
2024-09-19 12:26:01 +0900

    🐛Fix: url 오타 수정

--- miniproject/urls/production_urls.py
+++ miniproject/urls/production_urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from miniproject.urls.base_urls import urlpatterns as base_urlpatterns
 
 production_urlpatterns: list[str] = [
-    path("/users/", include("users.urls")),
-    path("/accounts/", include("accounts.urls")),
+    path("users/", include("users.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
 
 urlpatterns = base_urlpatterns + production_urlpatterns
